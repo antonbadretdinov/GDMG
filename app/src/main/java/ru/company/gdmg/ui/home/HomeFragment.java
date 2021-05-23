2d0706eb7e64773b305776687c72bc0dfdf4ccd0
Anton Badretdinov <antonbad03@gmail.com>
2021-05-24 02:08:02 +0700

    +fix clock

--- app/src/main/java/ru/company/gdmg/ui/home/HomeFragment.java
+++ app/src/main/java/ru/company/gdmg/ui/home/HomeFragment.java
@@ -1,7 +1,5 @@
 package ru.company.gdmg.ui.home;
 
-import android.app.TimePickerDialog;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +8,12 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-
-import ru.company.gdmg.MainActivity;
 import ru.company.gdmg.R;
 
 public class HomeFragment extends Fragment {
@@ -39,6 +27,7 @@ public class HomeFragment extends Fragment {
     }
 
     private boolean pressed = false;
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -46,17 +35,33 @@ public class HomeFragment extends Fragment {
         int hour;
         int minute;
         int date;
+
+        final TextView backText = view.findViewById(R.id.backfortxt);
+        final TextView hideText = view.findViewById(R.id.hideTime);
+        hideText.setVisibility(View.INVISIBLE);
         final TextView tv = view.findViewById(R.id.textView9);
         final TimePicker timePicker = view.findViewById(R.id.tp);
         timePicker.setIs24HourView(true);
-
+        hideText.setText(timePicker.getHour()+":"+timePicker.getMinute());
         timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
             @Override
             public void onTimeChanged(TimePicker timePicker, int hour, int minute) {
 
-                tv.setText("Ваш будильник будет установлен на "+hour+" : "+minute);
+                tv.setText("Ваш будильник будет установлен на ");
+                if(hour<10){
+                    hideText.setText("0"+hour+":"+minute);
+                    if(minute<10){
+                        hideText.setText("0"+hour+":"+"0"+minute);
+                    }
+                }else if(minute<10){
+                    hideText.setText(hour+":"+"0"+minute);
+                }else hideText.setText(hour+":"+minute);
             }
         });
+
+        hour = timePicker.getHour();
+        minute = timePicker.getMinute();
+
 
         final Button button = view.findViewById(R.id.startbutton);
         button.setOnClickListener(new View.OnClickListener() {
@@ -66,12 +71,17 @@ public class HomeFragment extends Fragment {
 
 
                 if(!pressed){
-                    tv.setText("Ваш будильник установлен на " +timePicker.getHour()+" : "+timePicker.getMinute());
+                    backText.setBackgroundColor(getResources().getColor(R.color.buttonPressed));
                     timePicker.setVisibility(View.INVISIBLE);
+                    hideText.setVisibility(View.VISIBLE);
+                    tv.setText("Ваш будильник установлен на ");
                     button.setBackgroundResource(R.drawable.buttonstylepressed);
                     button.setText("ОТКЛЮЧИТЬ");
                     pressed = true;
                 } else {
+                    backText.setBackgroundColor(getResources().getColor(R.color.main));
+                    hideText.setVisibility(View.INVISIBLE);
+                    tv.setText("Ваш будильник будет установлен на ");
                     timePicker.setVisibility(View.VISIBLE);
                     button.setBackgroundResource(R.drawable.button_style);
                     button.setText("СТАРТ");
